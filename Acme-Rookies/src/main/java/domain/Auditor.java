9c74b709636a7a13175fe2bc586d8cd34ac5062c
Raul <raulrodriguezmendez97@gmail.com>
2019-04-29 18:01:51 +0200

    update

--- Acme-Rookies/src/main/java/domain/Auditor.java
+++ Acme-Rookies/src/main/java/domain/Auditor.java
@@ -4,9 +4,26 @@ package domain;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Access(AccessType.PROPERTY)
 public class Auditor extends Actor {
+
+	private Position	position;
+
+
+	@NotNull
+	@Valid
+	@OneToOne(optional = false)
+	public Position getPosition() {
+		return this.position;
+	}
+
+	public void setPosition(final Position position) {
+		this.position = position;
+	}
 
 }
--- Acme-Rookies/src/main/java/domain/Position.java
+++ Acme-Rookies/src/main/java/domain/Position.java
@@ -12,7 +12,6 @@ import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -40,19 +39,7 @@ public class Position extends DomainEntity {
 	private int					isCancelled;
 	private Company				company;
 	private Collection<Problem>	problems;
-	private Auditor				auditor;
 
-
-	@NotNull
-	@Valid
-	@OneToOne(optional = false)
-	public Auditor getAuditor() {
-		return this.auditor;
-	}
-
-	public void setAuditor(final Auditor auditor) {
-		this.auditor = auditor;
-	}
 
 	@NotNull
 	@Valid
