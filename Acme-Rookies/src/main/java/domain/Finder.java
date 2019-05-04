9e705935ae1efee1e0333d52272459a432923d62
AntSegJim <antonio.alandalus97@gmail.com>
2019-05-04 10:31:18 +0200

    update finder

--- Acme-Rookies/src/main/java/domain/Finder.java
+++ Acme-Rookies/src/main/java/domain/Finder.java
@@ -7,7 +7,9 @@ import java.util.Date;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -16,6 +18,9 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(indexes = {
+	@Index(columnList = "keyWord, deadLine, minSalary, maxSalary")
+})
 public class Finder extends DomainEntity {
 
 	private String					keyWord;
